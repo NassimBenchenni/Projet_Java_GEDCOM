e084152bd1af600d2d4ffbfdf31453eeabcc689a
Nassim Benchenni <129753225+NassimBenchenni@users.noreply.github.com>
2025-12-14 15:06:37 +0100

    Delete src directory

--- src/GedcomException/GedcomException.java
+++
@@ -1,9 +0,0 @@
-// Classe Mère
-package GedcomException;
-
-@SuppressWarnings("serial")
-public class GedcomException extends Exception {
-    public GedcomException(String message) {
-        super(message);
-    }
-}
